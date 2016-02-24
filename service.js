0e91e2d4e05b61fc87992d9afa1f3950fd9078a7
Alex Larson <alarson85@gmail.com>
2016-02-24 13:44:44 -0600

    finished

--- service.js
+++ service.js
@@ -1,10 +1,17 @@
 var app = angular.module('userProfiles'); 
 
 app.service('mainService', function($http) {
+  // this.getUsers = function() {
+  //   return $http({
+  //       method: 'GET',
+  //       url: 'http://reqr.es/api/users?page=1'
+  //   })
+  // }
+
   this.getUsers = function() {
-    return $http({
-        method: 'GET',
-        url: 'http://reqr.es/api/users?page=1'
-    })
+  	return $http{{
+  		method: 'GET',
+  		url: 'http://reqr.es/api/users?page=1'
+  	}}
   }
 });
